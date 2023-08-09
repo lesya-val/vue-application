ee4cc21ed66595c62eeb484c5875c5808f1a3588
lesya-val <you@example.com>
2023-08-09 15:52:37 +0600

    feat: make post animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,13 @@
 <template>
 	<div v-if="posts.length > 0">
-		<PostItem 
-			v-for="post in posts" 
-			:key="post.id" 
-			:post="post"
-			@remove="$emit('remove', post)"
-		/>
+    <transition-group name="posts-list">
+      <PostItem
+          v-for="post in posts"
+          :key="post.id"
+          :post="post"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
 	</div>
 	<h2 
 		v-else
@@ -37,4 +39,20 @@ export default {
 	.post-list__empty {
 		color: red;
 	}
+  .posts-list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .posts-list-enter-active,
+  .posts-list-leave-active {
+    transition: all 0.5s ease;
+  }
+  .posts-list-enter-from,
+  .posts-list-leave-to {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  .posts-list-move {
+    transition: transform 0.5s ease;
+  }
 </style>
